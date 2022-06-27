bc176fbef6a1db2069f1a7b861dacdf2d7483ff3
emdemor <emdemor415@gmail.com>
2022-06-27 20:19:21 -0300

    imp: add one-hot-encode function to preprocessing module

--- src/model/preprocessing.py
+++ src/model/preprocessing.py
@@ -1,11 +1,58 @@
-from __future__ import annotations
-
 import numpy as np
 import pandas as pd
-from src.base.logger import loggin
+from src.base.logger import logging
 from src.base.commons import load_pickle, load_yaml
 from src.model.data import sanitize_features
 from src.model.features import build_features
+
+from src.global_variables import FEATURE_PARAMETERS_FILE
+
+
+def one_hot_encode(
+    X: pd.DataFrame,
+    column: str,
+    categories_to_drop: list or tuple = [],
+    sep: str = "_",
+) -> pd.DataFrame:
+    """Encode categorical features as a one-hot numeric array.
+
+    Parameters
+    ----------
+    X : pd.DataFrame
+        Pandas dataframe with the column to be encoded
+    column : str
+        Categorical column to encode
+    categories_to_drop : list or tuple
+        List or tuple of column names to be neglected on encode process.
+    sep : str, optional
+        Character between prefix and category on the new column name, by default '_'
+
+    Returns
+    -------
+    pd.DataFrame
+        Dataframe with the column encoded
+    """
+
+    feature_parameters = load_yaml(FEATURE_PARAMETERS_FILE)
+
+    try:
+        categories = feature_parameters[column]["encode"]["categories"]
+        drop_columns = feature_parameters[column]["encode"]["drop_columns"]
+
+        for category in categories:
+            if category not in drop_columns:
+                X[column + sep + category] = (X[column] == category).astype(int)
+
+        X = X.drop(columns=column)
+
+    except KeyError as err:
+        error_mesage = (
+            f"Column {err.__str__()} not found in file {FEATURE_PARAMETERS_FILE}."
+        )
+        logging.error(error_mesage)
+        raise KeyError(error_mesage)
+
+    return X
 
 
 def apply_preprocess(preprocessor, X: pd.DataFrame):
