ac7461e5c14e87a4fb1d9f045d90ebb904d02922
emdemor <emdemor415@gmail.com>
2022-06-27 22:11:40 -0300

    imp: add function build_features

--- src/model/preprocessing.py
+++ src/model/preprocessing.py
@@ -144,6 +144,32 @@ def apply_constant_imputes(X: pd.DataFrame) -> pd.DataFrame:
     return X
 
 
+def build_features(X: pd.DataFrame) -> pd.DataFrame:
+    """Cosntruct new feature from raw dataset
+
+    Parameters
+    ----------
+    X : pd.DataFrame
+        Input dataset
+
+    Returns
+    -------
+    pd.DataFrame
+        New dataframe with constructed features
+    """
+
+    X["V1_estado_civil"] = np.where(
+        (X["V1_estado_civil"] == "casado") | (X["V1_estado_civil"] == "divorciado"),
+        "caso_ou_divorciado",
+        X["V1_estado_civil"],
+    )
+
+    X["V1_tem_filhos"] = X["V1_qt_filhos"].clip(0, 1)
+    X = X.drop(columns="V1_tem_filhos")
+
+    return X
+
+
 def apply_preprocess(preprocessor, X: pd.DataFrame):
 
     X = preprocessor.transform(X)
