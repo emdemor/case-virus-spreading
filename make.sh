78cbac93e908a050a34e1030afc0f5145c2c705a
emdemor <emdemor415@gmail.com>
2022-06-30 00:06:55 -0300

    imp: add makefile

---
+++ make.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+echo "  _____                  _   _                               "
+echo " /  __ \                | \ | |                              "
+echo " | /  \/ __ _ ___  ___  |  \| | ___  _____      ____ _ _   _ "
+echo " | |    / _\` / __|/ _ \ | . \` |/ _ \/ _ \ \ /\ / / _\` | | | |"
+echo " | \__/\ (_| \__ \  __/ | |\  |  __/ (_) \ V  V / (_| | |_| |"
+echo "  \____/\__,_|___/\___| \_| \_/\___|\___/ \_/\_/ \__,_|\__, |"
+echo "                                                        __/ |"
+echo "                                                       |___/ "
+echo "Eduardo M. de Morais"
+
+echo "Setting conda environment"
+conda env create -n neoway --file environment.yml
+
+echo "Setting conda environment"
+conda activate neoway
+
+echo "Setting environment variables"
+export PYTHONPATH=$PYTHONPATH:$(pwd)
+
+echo "Setting directories"
+mkdir -p logs
+
+mkdir -p data/interim
+
+mkdir -p data/external
+
+mkdir -p data/predicted
+
+mkdir -p data/
+
+conda deactivate
+
+echo "Activate conda environment with 'conda activate neoway'"
